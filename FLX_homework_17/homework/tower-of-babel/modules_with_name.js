4bc975848b51e07ebbddfb38660136cc4829dee6
mykolamaksymivua <joker_maks@outlook.com>
2019-03-30 10:23:14 +0200

    Change import path

--- FLX_homework_17/homework/tower-of-babel/modules_with_name.js
+++ FLX_homework_17/homework/tower-of-babel/modules_with_name.js
@@ -1,7 +1,7 @@
 let arg1 = process.argv[2];
 let arg2 = process.argv[3];
 
-import {PI, sqrt, square} from './Math';
+import {PI, sqrt, square} from './modules_with_name_math.js';
 
 console.log(PI);
 console.log(sqrt(+arg1));
